22bf8a0307873a2a7e63a17ef9d41f61a4f289df
rahul gupta <rahulguptaslg20@gmail.com>
2023-12-09 01:48:41 +0530

    responsive 2

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -5,7 +5,7 @@ const SecondayContainer = () =>{
     const movies = useSelector(store => store.movies)
     return(
         <div className="bg-black ">
-            <div className="-mt-52  relative z-20 pl-12">
+            <div className="mt-0 md:-mt-52  relative z-20 pl-4 md:pl-12">
             <MovieList  title={"Now Playing"} movies={movies?.nowPlayingMovies}/>
             <MovieList  title={"Top Rated"} movies={movies?.topRatedMovies}/>
             <MovieList  title={"Upcoming Movies"} movies={movies?.upcomingMovies}/>
