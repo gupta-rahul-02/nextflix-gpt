13a4d50847f89a3d0e9a2f2f9f4e76de621379ce
rahul gupta <rahulguptaslg20@gmail.com>
2023-12-09 20:19:22 +0530

    Deployment

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,3 +1,29 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import {getAuth} from "firebase/auth"
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAi3D2j0l9MdVozqOSi_IhtQm_Zh3pe5Ms",
+//   authDomain: "netflixgpt-b4921.firebaseapp.com",
+//   projectId: "netflixgpt-b4921",
+//   storageBucket: "netflixgpt-b4921.appspot.com",
+//   messagingSenderId: "49468084126",
+//   appId: "1:49468084126:web:9e4b699463a598915a108f",
+//   measurementId: "G-CXQ9EXTDCX"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// export const auth = getAuth()
+
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -8,13 +34,13 @@ import {getAuth} from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAi3D2j0l9MdVozqOSi_IhtQm_Zh3pe5Ms",
-  authDomain: "netflixgpt-b4921.firebaseapp.com",
-  projectId: "netflixgpt-b4921",
-  storageBucket: "netflixgpt-b4921.appspot.com",
-  messagingSenderId: "49468084126",
-  appId: "1:49468084126:web:9e4b699463a598915a108f",
-  measurementId: "G-CXQ9EXTDCX"
+  apiKey: "AIzaSyDjuiBDltdDR9L1pXIYWWOkcIbBxr_97vk",
+  authDomain: "netflix-clone-b4610.firebaseapp.com",
+  projectId: "netflix-clone-b4610",
+  storageBucket: "netflix-clone-b4610.appspot.com",
+  messagingSenderId: "732171844032",
+  appId: "1:732171844032:web:679f88c0e27e3993103025",
+  measurementId: "G-VM8DMN29ZS"
 };
 
 // Initialize Firebase
